37ab8b5c8cbe37cfe8edc33274a8f7fdaf52fe66
vladyslavbezruk <vladyslavbezruk@yahoo.com>
2023-12-16 18:34:12 +0200

    Added new tests.

---
+++ src/test/java/edu/sumdu/tss/elephant/helper/utils/ExceptionUtilsTest.java
@@ -0,0 +1,24 @@
+package edu.sumdu.tss.elephant.helper.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExceptionUtilsTest {
+
+    @Test
+    void validationMessages() {
+    }
+
+    @Test
+    void isSQLUniqueException() {
+    }
+
+    @Test
+    void stacktrace() {
+    }
+
+    @Test
+    void wrapError() {
+    }
+}
---
+++ src/test/java/edu/sumdu/tss/elephant/helper/utils/MessageBundleTest.java
@@ -0,0 +1,16 @@
+package edu.sumdu.tss.elephant.helper.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageBundleTest {
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void testGet() {
+    }
+}
