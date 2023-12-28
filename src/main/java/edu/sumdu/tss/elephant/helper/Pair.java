8b3203409a250bbd7c8b3661d1e0a9c9835e30dc
SirBlaz333 <arseniyserebryakov17@gmail.com>
2023-12-28 17:40:30 +0100

    Fix code so it can be tested

--- src/main/java/edu/sumdu/tss/elephant/helper/Pair.java
+++ src/main/java/edu/sumdu/tss/elephant/helper/Pair.java
@@ -1,5 +1,7 @@
 package edu.sumdu.tss.elephant.helper;
 
+import java.util.Objects;
+
 public class Pair<T1, T2> {
     public T1 key;
     public T2 value;
@@ -18,5 +20,26 @@ public class Pair<T1, T2> {
 
     public T2 getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(key, pair.key) && Objects.equals(value, pair.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
     }
 }
